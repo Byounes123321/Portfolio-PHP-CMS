69a7c662c5790fa7f6c81ec243403db60ae7d3ec
wenhaolu <52440282+wenhao-lu@users.noreply.github.com>
2023-03-23 01:55:12 -0400

    add 'education'

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -17,6 +17,11 @@ include( 'includes/header.php' );
     </a>
   </li>
   <li>
+    <a href="education.php">
+      Manage Education
+    </a>
+  </li>
+  <li>
     <a href="users.php">
       Manage Users
     </a>
