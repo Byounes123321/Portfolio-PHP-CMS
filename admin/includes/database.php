403f0bf9fbe51763d41a419105d1cee41afc947c
Bassil Younes <bassilyounes@gmail.com>
2023-03-23 15:56:57 -0400

    updated gitignore

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -1,10 +1,5 @@
 <?php
 
-$connect = mysqli_connect( 
-    "host", // Host
-    "username", // Username
-    "password", // Password
-    "database" // Database
-);
+
 
 mysqli_set_charset( $connect, 'UTF8' );
